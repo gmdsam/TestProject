86f780c479f75577a1d676769bb00b675d292cbb
Saumil <saumilhariyani35@gmail.com>
2021-10-05 17:30:19 +0530

    updated read

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 class App:
-    def read_config():
-        pass
+    def __init__(self, env):
+        self.__env = env
+
+    def read_config(self):
+        DatabaseConfig.filename = os.path.join(self.__env, 'database.json')
     
+    def configure_services(self):
+        services = [
+            OCRService(),
+            ClassificationService(),
+            FeedbackService()
+        ]
+        for service in services:
+            service.run()
+
     def run():
         pass
 
 if __name__ == '__main__':
-    app = App()
+    app = App(os.getenviron('dev'))
     app.read_config()
     app.run()
